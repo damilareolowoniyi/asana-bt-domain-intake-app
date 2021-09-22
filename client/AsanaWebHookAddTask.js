246f36449b844c19c02f73e7da40902201ea9986
Dare Olowo <dolowoniyi@salesforce.com>
2021-09-22 19:49:23 +0100

    commiting target url for webhook

--- client/AsanaWebHookAddTask.js
+++ client/AsanaWebHookAddTask.js
@@ -1,33 +1,46 @@
 // Get your user info
-    
+
 var asana = require('asana');
-    
+
 // before deploying change this 
- var personalAccessToken =  process.env.ASANA_ACCESS_TOKEN;
+var personalAccessToken = process.env.ASANA_ACCESS_TOKEN;
 
 //local
 // var personalAccessToken = '1/1200191710492221:9c8c905608b0d38cebcf99e4b0791557';
 
 var client = asana.Client.create().useAccessToken(personalAccessToken);
 
-async function webHookAddNewTask(){
-       var webhookEvent = {
-        "data": {
-        "filters": [
-            {
-              "action": "added",
-               "resource_type": "task"
-            }
-          ],
-          "resource": "1200384595968979", // project id   
-          "target": "https://asana-bt-domain-intake-app.herokuapp.com/asana_create_new_task" // this is where your web-hooks 
-      }  
-    }
+async function webHookAddNewTask() {
+    //    var webhookEvent = {
+    //     "data": {
+    //     "filters": [
+    //         {
+    //           "action": "added",
+    //            "resource_type": "task"
+    //         }
+    //       ],
+    //       "resource": "1200384595968979",    
+    //       "target": "https://asana-bt-domain-intake-app.herokuapp.com/asana_create_new_task" // this is where your web-hooks 
+    //   }  
+    // }
+
+    const webhookEvent = {
+        data: {
+            filters: [{
+                action: "added",
+                resource_type: "task",
+            }, ],
+        },
+        resource: "1200384595968979", // This is the id of the section  
+        target: "https://asana-bt-domain-intake-app.herokuapp.com/asana_create_new_task" // this is where web-hooks script is
+    };
+
     client.webhooks.createWebhook(webhookEvent)
-        .then((result) => {
-         }).catch(function (err) {
+        .then((result) => {}).catch(function (err) {
             console.log(err.value.errors)
-          });
+        });
 }
 
-module.exports= {webHookAddNewTask};
+module.exports = {
+    webHookAddNewTask
+};
